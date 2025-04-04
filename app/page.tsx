1479a0ee587dcea0b6883e2bcb145052bfc00f6b
Sutharsika Kumar <sutharsikakumar@Sutharsikas-MacBook-Air-1503.local>
2025-04-03 22:20:44 -0400

    router v client match

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/navigation";
 import Hero from "@/components/hero";
 
 export default function Home() {
-  const router = useRouter();
   return (
     <>
     <Hero />
